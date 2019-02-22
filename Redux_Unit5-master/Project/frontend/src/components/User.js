69537ba0d9db3d6e9658951beb4489538d86e72d
jonelle <jonellebain@pursuit.org>
2019-02-22 17:08:53 -0500

    adding buttons

--- Redux_Unit5-master/Project/frontend/src/components/User.js
+++ Redux_Unit5-master/Project/frontend/src/components/User.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import '../css/User.css';
+import { Link } from 'react-router-dom'
 
+let plus = require('../assets/plus.png');
+let pencil = require('../assets/pencil.png')
+let share = require('../assets/share.png')
 
 export const User = ({ username }) => {
   return(
     <>
+  <div className="buttons">
+    <Link to={"/"}>
+    <img className='createPin' alt="logo" src={plus} width="40px" height="40px"/></Link>
+
+  </div>
+
   <div className="userContainer">
       <div className="userProfile">
         { username.username }
+
         <p className="followers"> 10 followers • 5 following </p>
       </div>
 
