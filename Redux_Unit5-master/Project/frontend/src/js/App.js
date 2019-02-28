ff3c124a36e2ee829de62657130465d32c70bbd5
jonelle <jonellebain@pursuit.org>
2019-02-28 17:57:38 -0500

    new version

---
+++ Redux_Unit5-master/Project/frontend/src/js/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import { matchPath } from "react-router";
+
+import Navbar from "./js/components/Navbar.js";
+import NavLogo from "./js/components/NavLogo.js";
+import NavSearch from "./js/components/NavSearch.js";
+import Home from "./js/components/Home.js";
+import Pins from './js/components/Pins.js'
+import Users from "./js/components/Users.js";
+import Boards from "./js/components/Boards.js";
+
+
+class App extends Component {
+  render () {
+    return()
+
+  }
+}
+
+export default App;
---
+++ Redux_Unit5-master/Project/frontend/src/js/components/Boards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Boards extends React.Component {
+  render () {
+    return(
+
+    )
+
+  }
+}
+
+export default Boards;
